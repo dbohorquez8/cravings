d21f2cda0fa46711d4671d2a7c8973c4386fba2b
Pedro Fernandez <pedro.fernandez@koombea.com>
2014-10-18 00:17:01 -0500

    Adding ActiveAdmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   namespace :mood do
     resources :tags, :only => [:index]
     resources :choices, :only => [:index]
