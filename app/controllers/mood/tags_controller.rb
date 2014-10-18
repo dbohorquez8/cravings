9a3169fedcbc9b3bda09235275b277288ca823f3
Pedro Fernandez <pedro.fernandez@koombea.com>
2014-10-18 03:41:09 -0500

    Cleaning session tags when going for the first time

--- app/controllers/mood/tags_controller.rb
+++ app/controllers/mood/tags_controller.rb
@@ -1,13 +1,18 @@
 class Mood::TagsController < ApplicationController
 
   def index
-    previous_tags = session[:tags].blank? ? [] : JSON.parse(session[:tags])
-    @tags = Mood.next_tags(previous_tags)
-    session[:tags] = (@tags + previous_tags).to_json
-
     respond_to do |format|
-      format.html
-      format.js { render json: @tags.to_json, layout: false }
+      format.html do
+        @tags = Mood.next_tags
+        session[:tags] = @tags.to_json
+      end
+
+      format.js do
+        previous_tags = session[:tags].blank? ? [] : JSON.parse(session[:tags])
+        @tags = Mood.next_tags(previous_tags)
+        session[:tags] = (@tags + previous_tags).to_json
+        render json: @tags.to_json, layout: false
+      end
     end
   end
 
