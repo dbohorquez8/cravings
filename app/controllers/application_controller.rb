cc89eeb06afe093dbdbf57157dd9eced5decf7bc
Pedro Fernandez <pedro.fernandez@koombea.com>
2014-10-19 18:17:38 -0500

    Root path after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
+    admin_root_path and return if resource.is_a? AdminUser
     meals_path
+
   end
 end
