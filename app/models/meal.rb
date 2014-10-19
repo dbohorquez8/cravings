16b5512bb5556b5c5b5e4d2f2a65cae231420a26
Pedro Fernandez <pedro.fernandez@koombea.com>
2014-10-19 12:57:29 -0500

    Guessing occasions for meals

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -3,4 +3,18 @@ class Meal < ActiveRecord::Base
   belongs_to :user
   belongs_to :choice
 
+  before_save :guess_occasion
+
+  OCCASIONS = {
+    breakfast: (5...11),
+    lunch: (11...16)
+  }
+
+  private
+
+  def guess_occasion
+    value = OCCASIONS.select{|key, value| value.include_with_range?(Time.now.hour)}
+    self.occasion = value ? value.keys.first.to_s : 'dinner'
+  end
+
 end
