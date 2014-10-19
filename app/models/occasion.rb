1f751e8a4e466d9d2e0f54d8f9a2c8b8a6904b31
Pedro Fernandez <pedro.fernandez@koombea.com>
2014-10-19 15:58:59 -0500

    fixing occasion guessing

--- app/models/occasion.rb
+++ app/models/occasion.rb
@@ -7,7 +7,7 @@ class Occasion
 
   def self.guess(time = Time.now, ranges = DEFAULT_OCCASIONS, default_occasion = 'dinner')
     value = ranges.select{|key, value| value.include_with_range?(time.hour)}
-    value ? value.keys.first.to_s : default_occasion
+    value.blank? ? value.keys.first.to_s : default_occasion
   end
 
 end
